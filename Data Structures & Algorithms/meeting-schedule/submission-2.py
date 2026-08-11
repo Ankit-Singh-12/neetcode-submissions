f9fa9996c4e5c01ec17402d3cec4007ee37b6f39
Ankit Singh <singhankit.as777@gmail.com>
2026-08-11 13:12:28 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        lastend = -math.inf
+        intervals.sort(key=lambda i:i.start)
+
+        for i in intervals:
+            if lastend <= i.start:
+                lastend = i.end
+            else:
+                return False
+
+        return True
